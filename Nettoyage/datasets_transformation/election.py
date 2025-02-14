f60c28b1f212409ade3e1cab694931976d54390a
thomas.deschampt@epsi.fr <thomas.deschampt@epsi.fr>
2025-02-14 15:10:25 +0100

    creation du csv election et nettoyage

---
+++ Nettoyage/datasets_transformation/election.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os 
+
+def nettoyer_election(fichier_entree,dossier_sortie):
+    try:
+
+        df = pd.read_csv(fichier_entree, delimiter = ';' , dtype = str)
+        print(df.info())
+        df = df.drop_duplicates()
+
+        # Remplacement des virgules par des points et conversion en float
+        df = df.apply(lambda x: x.str.replace(',', '.', regex=False) if x.dtype == "object" else x)
+        df = df.apply(lambda x: pd.to_numeric(x, errors='ignore'))
+
+        # Création du nouveau csv nettoyé
+        dossier_sortie = "Nettoyage/datasets_nettoyer"
+        if not os.path.exists(dossier_sortie):
+            os.makedirs(dossier_sortie)
+
+        nom_fichier = "elections_nettoyer.csv"
+        chemin_sortie = os.path.join(dossier_sortie,nom_fichier)
+        df.to_csv(chemin_sortie, index=False, sep=';')
+
+    except Exception as e:
+        print(e)
