6410f8c70a862a52b295710249a1eff7c0a1f94c
Thomas Deschampt <thomas.deschampt@epsi.fr>
2025-03-28 10:11:33 +0100

    sauvergardes des données propres

--- Prediction/preparation/preparation.py
+++ Prediction/preparation/preparation.py
@@ -12,9 +12,19 @@ def preparation():
     # On encode les colonnes catégorielles
     df = pd.get_dummies(df, columns=categorical_columns)
 
-    # On normalise les données
-    scaler = StandardScaler()
-    df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
+    # On fait un lissage sur la colonne Ratio_voix_exprime en groupant par Tour et Annee
+    df['Ratio_voix_exprime'] = df.groupby(['Tour', 'Annee'])['Ratio_voix_exprime'].transform(lambda x: x.rolling(window=3, min_periods=1, center=True).mean().round(2))
+
 
     # On supprime les années sans election
     df = df.dropna(subset=['Tour'])
+
+    # On supprime les colonnes qui commence par 'Parti'
+    df = df[df.columns.drop(list(df.filter(regex='Parti')))]
+
+    # Affichage de la corrélation avec la colonne Ratio_voix_exprime dans l'ordre décroissant
+    pd.set_option('display.max_rows', None)
+    print(df.corr()['Ratio_voix_exprime'].sort_values(ascending=False))
+
+    # On sauvegarde les données préparées
+    df.to_csv('./Prediction/data/donnees_preparees.csv', index=False)
